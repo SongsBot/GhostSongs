f5eaef97bf0027860b2958ca8d43517feb67b340
SongsBot <41879762+SongsBot@users.noreply.github.com>
2018-07-31 02:33:47 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+ const prefix = "*";
 client.on('ready', () => {
     console.log('I am ready!');
 });
@@ -12,37 +13,148 @@ client.on('message', message => {
 
 
 
+client.on('voiceStateUpdate', (old, now) => {
+  const channel = client.channels.get('472875486700240906');
+  const currentSize = channel.guild.members.filter(m => m.voiceChannel).size;
+  const size = channel.name.match(/\[\s(\d+)\s\]/);
+  if (!size) return channel.setName(`Voice Online [ ${currentSize} ]`);
+  if (currentSize !== size) channel.setName(`Voice Online [ ${currentSize} ]`);
+});
+
+client.on('message', message => {
+    if (message.content.startsWith("رابط")) {
+
+  message.channel.createInvite({
+        thing: true,
+        maxUses: 100,
+        maxAge: 86400
+    }).then(invite =>
+      message.author.sendMessage(invite.url)
+    )
+    const embed = new Discord.RichEmbed()
+        .setColor("RANDOM")
+        .setDescription(" ✅    تم ارسال الرابط على الخاص  ")
+      message.channel.sendEmbed(embed).then(message => {message.delete(10000)})
+              const Embed11 = new Discord.RichEmbed()
+        .setColor("RANDOM")
+                .setAuthor(message.guild.name, message.guild.iconURL)
+        .setDescription(`
+**
+
+-${message.guild.name}  Link
+**`)
+      message.author.sendEmbed(Embed11)
+    }
+});
+ 
+ 
+ var dat = JSON.parse("{}");
+function forEachObject(obj, func) {
+    Object.keys(obj).forEach(function (key) { func(key, obj[key]) });
+}
+client.on("ready", () => {
+    var guild;
+    while (!guild)
+        guild = client.guilds.get("359674601392832512");
+    guild.fetchInvites().then((data) => {
+        data.forEach((Invite, key, map) => {
+            var Inv = Invite.code;
+            dat[Inv] = Invite.uses;
+        });
+    });
+});
 
 
-const Music = require('discord.js-musicbot-addon');
-client.login(`NDczMjk1Mzk4NjIyOTg2MjQw.Dj_2cA.eB_6AffFEZCRs4NqOnzGviGVQQs`)
-    console.log('Ready!')
+
+client.on("guildMemberAdd", (member) => {
+    let channel = member.guild.channels.get("471393414395461634");
+    if (!channel) {
+        console.log("!the channel id it's not correct");
+        return;
+    }
+    if (member.id == client.user.id) {
+        return;
+    }
+    console.log('-');
+    var guild;
+    while (!guild)
+        guild = client.guilds.get("359674601392832512");
+    guild.fetchInvites().then((data) => {
+        data.forEach((Invite, key, map) => {
+            var Inv = Invite.code;
+            if (dat[Inv])
+                if (dat[Inv] < Invite.uses) {
+ channel.send(`تم دعوته بواسطة  ${Invite.inviter} `) ;         
+ }
+            dat[Inv] = Invite.uses;
+       
+       });
+    });
+});
+
+
+  client.on('message', message => {
+            if(!message.channel.guild) return;
+let args = message.content.split(' ').slice(1).join(' ');
+if (message.content.startsWith('>bcall')){
+ if (message.author.id !== '321476121956974595') return message.reply('** هذا الأمر قفط لصاحب البوت و شكراًً **')
+ if(!message.author.id === '321476121956974595') return;
+message.channel.sendMessage('جار ارسال الرسالة |✅')
+client.users.forEach(m =>{
+m.sendMessage(args)
 })
- Music.start(client, {
-    prefix: "+", // البرفكس هنا
-    youtubeKey: 'AIzaSyDIv8O9OwZdoTaghK3_SQmyGcQFzkXMW6E', //هنا ال yt v3 api key
-    global: false, // يكون للسيرفرات ؟ خليه false, يكون queue مشترك خليه true            
-    maxQueueSize: 50, // اقصى حجم لل queue        
-    playCmd: 'play', // امر التشغيل
-    volumeCmd: 'vol',     // امر الصوت
-    thumbnailType: 'high',
-    leaveCmd: 'stop',    // امر المغادرة
-    anyoneCanSkip: true, // تبي الكل يقدر يسوي skip? true > yep, false > nope
-    disableLoop: false, // تبي يكون فيه اعادة؟ , true > yep, false > nope
-    searchCmd: 'search', //امر البحث 
-    requesterName: true, 
-    inlineEmbeds: false,     
-    queueCmd: 'queue', // امر القائمة
-    pauseCmd: 'pause', // امر الايقاف المؤقت
-    resumeCmd: 'resume', // امر الاستمرار
-    skipCmd: 'skip', // امر السكب
-    loopCmd: 'loop', // امر الاعادة
-    enableQueueStat: true,
-  });
+}
+});
 
 
 
 
+client.on('ready', function(){
+    var ms = 10000 ;
+    var setGame = [`Bot ON ${client.guilds.size} Servers`,`In Server ${client.users.size} Users`];
+    var i = -1;
+    var j = 0;
+    setInterval(function (){
+        if( i == -1 ){
+            j = 1;
+        }
+        if( i == (setGame.length)-1 ){
+            j = -1;
+        }
+        i = i+j;
+        client.user.setGame(setGame[i],`http://www.twitch.tv/KiNg66S`);
+    }, ms);
 
+});
+
+client.on('guildMemberAdd', member => {
+    var embed = new Discord.RichEmbed()
+    .setAuthor(member.user.username, member.user.avatarURL)
+    .setThumbnail(member.user.avatarURL)
+    .setTitle(`عضو جديد`)
+    .setDescription(`اهلا بك في السيرفر منور :heart:`)
+    .addField(' :bust_in_silhouette:  انت رقم',`**[ ${member.guild.memberCount} ]**`,true)
+    .setColor('RED')
+    .setFooter('GhostGamers', 'https://images-ext-2.discordapp.net/external/isSXO_JfjdAVla24z1lzOgnoiCHzALglHXEZ70kmMvk/https/cdn.pg.sa/OD53Wjr9p0.gif')
+
+var channel =member.guild.channels.find('name', 'public-chat')
+if (!channel) return;
+channel.send({embed : embed});
+});
+
+client.on('message', message => {
+  if (message.author.bot) return;
+  if (!message.content.startsWith(prefix)) return;
+
+  let command = message.content.split(" ")[0];
+  command = command.slice(prefix.length);
+
+  let args = message.content.split(" ").slice(1);
+
+  if (command == "~say") {
+   message.channel.sendMessage(args.join("  "))
+   message.delete()
+  }
+ });
 
 client.login(process.env.BOT_TOKEN);
