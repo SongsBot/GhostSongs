8a28fab385c9ba5eaecd2568926c6a1eddb91bff
SongsBot <41879762+SongsBot@users.noreply.github.com>
2018-07-30 04:48:22 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -19,7 +19,7 @@ client.on('message', message => {
     client.user.setStatus("idle")
 });
 //by ,$ ReBeL ء , 🔕#4777 'CODES SERVER'
-const prefix = "$"
+const prefix = "+"
 client.on('message', async msg => { // eslint-disable-line
 	if (msg.author.bot) return undefined;
 	//by ,$ ReBeL ء , 🔕#4777 'CODES SERVER'
